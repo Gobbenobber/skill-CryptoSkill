a8f351eb3fc7bdc5b0e5cbfcf54b2e67f78348e3
Gobbenobber <gobbenobber@gmail.com>
2018-01-31 18:53:24 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -100,4 +100,4 @@ class CryptoSkill(MycroftSkill):
 # The "create_skill()" method is used to create an instance of the skill.
 # Note that it's outside the class itself.
 def create_skill():
-    return SurfaceControl()
+    return CryptoSkill()
