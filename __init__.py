a8f6fcfb4d27a91feebd1ea2f8552e587a79b3f2
Gobbenobber <gobbenobber@gmail.com>
2018-02-05 12:40:33 +0100

    ...

--- __init__.py
+++ __init__.py
@@ -88,9 +88,11 @@ class CryptoSkill(MycroftSkill):
         self.speak_dialog("TopThreeCoins")
         TopThreeCoins = requests.get("https://api.coinmarketcap.com/v1/ticker/?limit=3").json()
         for x in TopThreeCoins:
-        	self.speak("The %d. most valuable coin is: " % (x+1)) 
-        	self.speak(TopThreeCoins[x]["name"])
-        	self.speak("Which currently has a market value of:")
+            sproken = "The %d. most valuable coin is: " % (x+1)
+            LOGGER.error("{0}".format(sproken))
+            self.speak(sproken)
+            self.speak(TopThreeCoins[x]["name"])
+            self.speak("Which currently has a market value of:")
         	self.speak(TopThreeCoins[x]["price_usd"])
         	self.speak("US dollars per coin.")
        
