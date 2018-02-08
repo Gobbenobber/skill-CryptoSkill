eb4fb49b5fbac7a8851690b43aeebdced144e99f
Gobbenobber <gobbenobber@gmail.com>
2018-02-08 14:09:07 +0100

    happns

--- __init__.py
+++ __init__.py
@@ -93,8 +93,8 @@ class CryptoSkill(MycroftSkill):
         self.speak_dialog("MarketCap")
         data = requests.get("https://api.coinmarketcap.com/v1/global/").json()
         print(data)
-        self.speak(str(data["total_market_cap_usd"]/1000))
-        self.speak("Million US dollars.")
+        self.speak(str(data["total_market_cap_usd"][:3]))
+        self.speak("billion US dollars.")
     
     def handle_TopThreeCoins_Intent(self, message):
         self.speak_dialog("TopThreeCoins")
